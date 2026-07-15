4090e8f2810b7ee9ea1bd8e280ab8e22355370b5
romfir <github@romfir.email>
2026-07-15 15:59:48 +0200

    feat: Added `OpenApiHttpClient` + bumped packages

--- OpenApiLINQPadDriver/OpenApiContextDriver.cs
+++ OpenApiLINQPadDriver/OpenApiContextDriver.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using LINQPad.Extensibility.DataContext;
+#if DEBUG_PUBLISH_TO_LINQPAD_FOLDER
+using System;
+using System.Diagnostics;
+#endif
 
 namespace OpenApiLINQPadDriver;
-// ReSharper disable once UnusedMember.Global
+// ReSharper disable once UnusedType.Global
 public class OpenApiContextDriver : DynamicDataContextDriver
 {
 #if DEBUG_PUBLISH_TO_LINQPAD_FOLDER
@@ -31,9 +34,7 @@ public class OpenApiContextDriver : DynamicDataContextDriver
         var dialogProperties = new OpenApiContextDriverProperties(cxInfo);
 
         if (new ConnectionDialog(dialogProperties).ShowDialog() != true)
-        {
             return false;
-        }
 
         cxInfo.Persist = dialogProperties.Persist;
         cxInfo.IsProduction = dialogProperties.IsProduction;
@@ -44,9 +45,12 @@ public class OpenApiContextDriver : DynamicDataContextDriver
 
     public override string Name => DriverName;
 
-    // ReSharper disable StringLiteralTypo
-    public override string Author => "Damian Romanowski (romfir22@gmail.com)";
-    // ReSharper restore StringLiteralTypo
+    public override string Author =>
+#pragma warning disable CS8603 // Possible null reference return.
+        typeof(OpenApiContextDriver).Assembly
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .Single(a => a.Key == "Authors").Value;
+#pragma warning restore CS8603 // Possible null reference return.
 
     public override List<ExplorerItem> GetSchemaAndBuildAssembly(IConnectionInfo cxInfo, AssemblyName assemblyToBuild, ref string nameSpace, ref string typeName)
         => SchemaBuilder.GetSchemaAndBuildAssembly(new OpenApiContextDriverProperties(cxInfo), assemblyToBuild, ref nameSpace, ref typeName);
@@ -58,16 +62,14 @@ public class OpenApiContextDriver : DynamicDataContextDriver
         ];
 
     public override object[] GetContextConstructorArguments(IConnectionInfo cxInfo)
-        => [new HttpClient()];
+        => [new OpenApiHttpClient()];
 
     public override void InitializeContext(IConnectionInfo cxInfo, object context, QueryExecutionManager executionManager)
     {
     }
 
     public override void TearDownContext(IConnectionInfo cxInfo, object context, QueryExecutionManager executionManager, object[] constructorArguments)
-    {
-        ((HttpClient)constructorArguments[0]).Dispose();
-    }
+        => ((HttpClient)constructorArguments[0]).Dispose();
 
     public override bool AreRepositoriesEquivalent(IConnectionInfo c1, IConnectionInfo c2)
        => OpenApiContextDriverPropertiesEqualityComparer.Default.Equals(new OpenApiContextDriverProperties(c1), new OpenApiContextDriverProperties(c1));
---
+++ OpenApiLINQPadDriver/OpenApiHttpClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace OpenApiLINQPadDriver;
+
+public sealed class OpenApiHttpClient : HttpClient
+{
+    private readonly ConfigurableHttpMessageHandler _handler;
+
+    public OpenApiHttpClient() : this(new ConfigurableHttpMessageHandler())
+    {
+    }
+
+    private OpenApiHttpClient(ConfigurableHttpMessageHandler handler) : base(handler, disposeHandler: true)
+        => _handler = handler;
+
+    // ReSharper disable once UnusedMember.Global
+    public void ConfigureTransportOnce(Func<HttpMessageHandler> handlerFactory)
+        => _handler.ConfigureOnce(handlerFactory);
+}
